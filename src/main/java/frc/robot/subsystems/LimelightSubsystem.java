8e6d19382364af563d7d685fb812b5082993cefe
bakedPotatoLord <918780@dpsk12.net>
2023-03-28 15:30:19 -0600

    move limelight defaultCommand to it's own class

--- src/main/java/frc/robot/subsystems/LimelightSubsystem.java
+++ src/main/java/frc/robot/subsystems/LimelightSubsystem.java
@@ -8,28 +8,41 @@ package frc.robot.subsystems;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class LimelightSubsystem extends SubsystemBase {
-    NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
-    NetworkTableEntry tx = table.getEntry("tx");
-    NetworkTableEntry ty = table.getEntry("ty");
-    NetworkTableEntry ta = table.getEntry("ta");
+	NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+	NetworkTableEntry tx = table.getEntry("tx");
+	NetworkTableEntry ty = table.getEntry("ty");
+	NetworkTableEntry ta = table.getEntry("ta");
 
-    public LimelightSubsystem() {
-        System.out.println("Limelight started");
-    }
+	public LimelightSubsystem() {
+		System.out.println("Limelight started");
 
-    public double getX() {
-        return tx.getDouble(0);
-    }
+		setDefaultCommand(
+			new RunCommand(    
+				() -> {
+					SmartDashboard.putNumber("x", getX());
+					SmartDashboard.putNumber("y", getY());
+					SmartDashboard.putNumber("a", getA());
+				}
+			, 
+			this
+		)
+		);
+	}
 
-    public double getY() {
-        return ty.getDouble(0);
-    }
+	public double getX() {
+		return tx.getDouble(0);
+	}
 
-    public double getA() {
-        return ta.getDouble(0);
-    }
+	public double getY() {
+		return ty.getDouble(0);
+	}
 
+	public double getA() {
+		return ta.getDouble(0);
+	}
 }
