4c7634e76052c211ee554b4e5bff1e13334d6e31
bakedPotatoLord <918780@dpsk12.net>
2023-03-28 12:22:57 -0600

    turn toggleCompressor into instantCommand

--- src/main/java/frc/robot/subsystems/PneumaticsSubsytem.java
+++ src/main/java/frc/robot/subsystems/PneumaticsSubsytem.java
@@ -5,6 +5,7 @@ import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticHub;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class PneumaticsSubsytem extends SubsystemBase{
@@ -21,16 +22,20 @@ public class PneumaticsSubsytem extends SubsystemBase{
     return compressor;
   }
 
-  public void toggleCompressor(){
-    if(compressor.isEnabled()){
-      compressor.disable();
-      // display Compressor Data
-      SmartDashboard.putData(compressor);
-
-    }else{
-      compressor.enableDigital();
-      SmartDashboard.putData(compressor);
-    }
+  public InstantCommand toggleCompressor(){
+    return new InstantCommand(
+      () -> {
+        if(compressor.isEnabled()){
+          compressor.disable();
+          // display Compressor Data
+          SmartDashboard.putData(compressor);
+    
+        }else{
+          compressor.enableDigital();
+          SmartDashboard.putData(compressor);
+        }
+      }
+    );
   }
 
   public DoubleSolenoid makeDoubleSolenoid(int fwdPort,int revPort){
