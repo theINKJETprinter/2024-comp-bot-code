c3b2e13d545989356955757ac0c3484e2064ec1e
bakedPotatoLord <918780@dpsk12.net>
2023-03-28 16:58:45 -0600

    switch to subsystem commands

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -24,22 +24,14 @@ public class RobotContainer {
   final LimelightSubsystem limeLight = new LimelightSubsystem();
   final PneumaticsSubsytem pneumatics = new PneumaticsSubsytem();
   final DriveSubsystem m_driveSubsystem = new DriveSubsystem();
-  final IntakeSubsystem m_intakeSubsystem = new IntakeSubsystem(pneumatics);
-  final Bucket m_bucketSubsystem = new Bucket(pneumatics);
-  final ToggleCompressor toggleCompressor = new ToggleCompressor(pneumatics);
+  final IntakeSubsystem intake = new IntakeSubsystem(pneumatics);
+  final Bucket bucket = new Bucket(pneumatics);
   final Gyro gyro = new Gyro();
 
-  final LimelightCommand limelightCommand = new LimelightCommand(limeLight);
-  final IntakeCommand runIntake = new IntakeCommand(m_intakeSubsystem, Constants.intake.fwdSpeed);
-  final IntakeCommand runIntakeBackward = new IntakeCommand(m_intakeSubsystem, Constants.intake.revSpeed);
-  final ToggleBucketCommand toggleBucket = new ToggleBucketCommand(m_bucketSubsystem);
-  final IntakeToggleCommand toggleIntake = new IntakeToggleCommand(m_intakeSubsystem);
-
-  final AutoSelector selector = new AutoSelector(m_driveSubsystem, m_intakeSubsystem, m_bucketSubsystem, gyro);
+  final AutoSelector selector = new AutoSelector(m_driveSubsystem, intake, bucket, gyro);
 
   final CommandXboxController movementJoystick = new CommandXboxController(Constants.MOVEMENT_JOYSTICK);
   final CommandXboxController manipulatorJoystick = new CommandXboxController(Constants.MANIPULATOR_JOYSTICK);
-
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
@@ -47,7 +39,6 @@ public class RobotContainer {
     configureButtonBindings();
     
     //start cameraServer
-    CameraServer.startAutomaticCapture();
     CameraServer.startAutomaticCapture();
     
     m_driveSubsystem.setDefaultCommand(
@@ -57,28 +48,27 @@ public class RobotContainer {
             () -> (-movementJoystick.getLeftX() )
       ));
 
-
-    limeLight.setDefaultCommand(limelightCommand);
-
     gyro.log();
 
   }
 
   private void configureButtonBindings() {
     manipulatorJoystick.leftBumper() //intake
-    .whileTrue(runIntake);
-
-    manipulatorJoystick.rightBumper()//outake
-    .whileTrue(runIntakeBackward);
+    .onTrue(intake.setSpeed(Constants.intake.fwdSpeed))
+    .onFalse(intake.stopMotors());
+    
+    manipulatorJoystick.rightBumper()//eject
+    .onTrue(intake.setSpeed(Constants.intake.revSpeed))
+    .onFalse(intake.stopMotors());
 
     manipulatorJoystick.x()
-    .onTrue(toggleBucket);
+    .onTrue(bucket.toggle());
 
     manipulatorJoystick.a()
-    .onTrue(toggleIntake);
+    .onTrue(intake.togglePiston());
 
     manipulatorJoystick.y()
-    .onTrue(toggleCompressor);
+    .onTrue(pneumatics.toggleCompressor());
   }
 
   // Auto Stuff
